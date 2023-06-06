5cfee154db18484e74a2ce6a1021fd5805bf4fd7
baghsaurabh <119872158+baghsaurabh@users.noreply.github.com>
2023-06-06 14:17:01 +0200

    may update

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { environment } from '../../environments/development';
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
@@ -12,13 +12,15 @@ export class Page1Component implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    const apiUrl = environment.apiUrl;
+
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:4200'
+        'Access-Control-Allow-Origin': apiUrl
       })
     };
-    this.http.get<any[]>('https://localhost:7198/api/Products', httpOptions)
+    this.http.get<any[]>(apiUrl, httpOptions)
       .subscribe(products => {
         this.products = products;
       });
---
+++ src/environments/development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://20.219.231.148/api/Products'
+  };
